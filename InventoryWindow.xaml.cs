f43e1852672ea9d29053acc0d98615c38c452982
Frotekz <s.kashuashvili@tug.ge>
2022-01-29 22:56:49 +0400

    uploading

---
+++ InventoryWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmartInventory
+{
+    /// <summary>
+    /// Interaction logic for InvetoryWindow.xaml
+    /// </summary>
+    public partial class InvetoryWindow : Window
+    {
+        public InvetoryWindow()
+        {
+            InitializeComponent();
+            LoadGrid();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=Frotek;Initial Catalog=Products; Integrated Security=True");
+        public void LoadGrid()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("select * from prodList", con);
+                DataTable dt = new DataTable();
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                dt.Load(sdr);
+                con.Close();
+                datagrid.ItemsSource = dt.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void clearData()
+        {
+            txtTitle.Clear();
+            txtCategory.Clear();
+            txtWarranty.Clear();
+            txtPrice.Clear();
+            txtSearch.Clear();
+        }
+
+        public bool isValid()
+        {
+            if (txtTitle.Text == string.Empty)
+            {
+                MessageBox.Show("Title is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (txtCategory.Text == string.Empty)
+            {
+                MessageBox.Show("Category is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (prodDate.Text == string.Empty)
+            {
+                MessageBox.Show("Production date is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (txtWarranty.Text == string.Empty)
+            {
+                MessageBox.Show("Warranty is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (txtPrice.Text == string.Empty)
+            {
+                MessageBox.Show("Price is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update prodList set title= '" + txtTitle.Text + "', category= '" + txtCategory.Text + "', prodDate= '" + prodDate.SelectedDate + "', warranty= '" + txtWarranty.Text + "', price= '" + txtPrice.Text + "' WHERE ID= '" + txtSearch.Text + "' ", con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record has been successfully updated","Updated",MessageBoxButton.OK,MessageBoxImage.Information);
+
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+                clearData();
+                LoadGrid();
+            }
+        }
+
+        private void btnDelete_Click(object sender,RoutedEventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from prodList where ID=" +txtSearch.Text+" ",con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record has been deleted","Deleted",MessageBoxButton.OK,MessageBoxImage.Exclamation);
+                con.Close();
+                clearData();
+                LoadGrid();
+                con.Close();
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show("Item not deleted",ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (isValid())
+                {
+                    int nWarranty = Convert.ToInt32(txtWarranty.Text);
+                    int nPrice = Convert.ToInt32(txtPrice.Text);
+                    SqlCommand cmd = new SqlCommand("INSERT INTO prodList VALUES(@title,@category,@prodDate,@warranty,@price)");
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@title", txtTitle.Text);
+                    cmd.Parameters.AddWithValue("@category", txtCategory.Text);
+                    cmd.Parameters.AddWithValue("@prodDate", prodDate.SelectedDate);
+                    cmd.Parameters.AddWithValue("@warranty",nWarranty);
+                    cmd.Parameters.AddWithValue("@price", nPrice);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    LoadGrid();
+                    MessageBox.Show("Successfuly added", "Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                    clearData();
+                }
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ LoginForm.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmartInventory
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class LoginFrom : Window
+    {
+        public LoginFrom()
+        {
+            InitializeComponent();
+        }
+
+        static bool ValidatePassword(string passWord)
+        {
+            int validConditions = 0;
+            foreach (char c in passWord)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    validConditions++;
+                    break;
+                }
+            }
+            foreach (char c in passWord)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    validConditions++;
+                    break;
+                }
+            }
+            if (validConditions == 0) return false;
+            foreach (char c in passWord)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    validConditions++;
+                    break;
+                }
+            }
+            if (validConditions == 1) return false;
+            if (validConditions == 2)
+            {
+                char[] special = { '@', '#', '$', '%', '^', '&', '+', '=' }; // or whatever    
+                if (passWord.IndexOfAny(special) == -1) return false;
+            }
+            return true;
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=FROTEK;Initial Catalog=LoginDB; Integrated Security=True;");
+
+            if (ValidatePassword(txtPassword.Password))
+            {
+                try
+                {
+                    if (sqlCon.State == System.Data.ConnectionState.Closed)
+                        sqlCon.Open();
+                    String query = "SELECT COUNT(1) FROM userTable WHERE Username=@Username AND Password=@Password";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.CommandType = CommandType.Text;
+                    sqlCmd.Parameters.AddWithValue("@UserName", txtUsername.Text);
+                    sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                    int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    if (count == 1)
+                    {
+                        MainWindow dashboard = new MainWindow();
+                        dashboard.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username or password is incorrect");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlCon.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Your password didn't contain required characters");
+            }
+        }
+    }
+}
